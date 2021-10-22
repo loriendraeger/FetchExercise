02f9da46967dfa3d16134e6ff2fd535457c5b605
loriendraeger <loriendraeger@gmail.com>
2021-10-22 13:50:05 -0500

    Add models

---
+++ FetchExercise/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace FetchExercise.Models
+{
+    /*
+     * In this data model, everything is stored as a Transaction.
+     * Each transaction also represents a batch of points that can be consumed (stored in PointsRemaining).
+     * 
+     * Positive transactions generally start with PointsRemaining equal to Points and then PointsRemaining
+     * is consumed by negative transactions. Negative transactions will typically have a null PointsRemaining.
+     * 
+     * However, if we receive a negative transaction and there are corresponding positive transactions for
+     * it to consume, we mark note that it is outstanding by setting a negative PointsRemaining.
+     * 
+     * All of this logic is handled in the TransactionController Post method.
+     */
+
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string Payer { get; set; }
+        public long Points { get; set; }
+        public DateTime Timestamp { get; set; }
+        public long? PointsRemaining { get; set; }
+
+        public Transaction()
+        {
+        }
+    }
+}
---
+++ FetchExercise/Models/TransactionContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace FetchExercise.Models
+{
+    public class TransactionContext : DbContext
+    {
+        public TransactionContext(DbContextOptions<TransactionContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
